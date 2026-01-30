1c905ca2cf0e58d700364d3e22d839d59d136f99
Alexander Schuetz <aschuetz@protonmail.com>
2026-01-30 20:34:14 +0100

    fix linux impl add openbsd ci

--- src/platform_impl/linux.rs
+++ src/platform_impl/linux.rs
@@ -1,5 +1,3 @@
-use libc::{c_long, getpid, syscall};
-
 pub fn is_main_thread() -> Option<bool> {
-    Some(unsafe { syscall(SYS_gettid) == getpid() as c_long })
+    Some(unsafe { libc::gettid() == libc::getpid() })
 }
