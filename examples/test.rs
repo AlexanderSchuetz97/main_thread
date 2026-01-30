f90f967eafb13e7f76bdf5b4efcb72463582ded1
Alexander Schuetz <aschuetz@protonmail.com>
2026-01-30 20:26:08 +0100

    remove debug code from test example

--- examples/test.rs
+++ examples/test.rs
@@ -1,9 +1,6 @@
 use main_thread::{is_main_thread, IsMainThread};
 
 fn main() {
-    let n = unsafe { libc::getpid() };
-    println!("pthread_self = {}", n as isize);
-
     let is_main = std::thread::spawn(|| is_main_thread()).join().unwrap();
 
     assert_ne!(is_main, IsMainThread::MainThread);
