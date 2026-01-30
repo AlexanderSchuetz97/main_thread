9be43f40742637e6076d9d0740eec95331f74cdf
Alexander Schuetz <aschuetz@protonmail.com>
2026-01-30 18:40:45 +0100

    add macos ci test

--- dll_test_crate/examples/test.rs
+++ dll_test_crate/examples/test.rs
@@ -1,6 +1,6 @@
 use std::{thread};
 
-#[cfg(not(windows))]
+#[cfg(all(not(windows), not(target_vendor = "apple")))]
 pub fn main() {
     unsafe {
         let lib = thread::spawn(|| {libloading::Library::new("target/debug/libdll_test_crate.so").unwrap()}).join().unwrap();
@@ -15,6 +15,21 @@ pub fn main() {
         }).join().unwrap();
         assert_eq!(2, x2);
     }
+}
+
+#[cfg(target_vendor = "apple")]
+pub fn main() {
+    let lib = thread::spawn(|| {libloading::Library::new("target/debug/libdll_test_crate.dynlib").unwrap()}).join().unwrap();
+
+    let symbol = lib.get::<extern "C" fn() -> u32>("is_this_the_main_thread").unwrap().try_as_raw_ptr().unwrap() as usize;
+    std::mem::forget(lib);
+
+    assert_eq!(1, std::mem::transmute::<usize, extern "C" fn() -> u32>(symbol)());
+
+    let x2 = thread::spawn(move || {
+        std::mem::transmute::<usize, extern "C" fn() -> u32>(symbol)()
+    }).join().unwrap();
+    assert_eq!(2, x2);
 }
 
 #[cfg(target_os = "windows")]
